2a7d0607c15e72043ad670717cda23e4bd917747
Sidharth3011Gupta <sidharthgupta004@gmail.com>
2024-09-24 12:53:52 +0530

    cors

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const bodyParser = require('body-parser');
 const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: ['https://todo-app-frontend-teal-two.vercel.app/',],
-    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH','OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
- }))
+// app.use(cors({
+//     credentials: true,
+//     origin: ['https://todo-app-frontend-teal-two.vercel.app/',],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH','OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+//  }))
 
- app.options('*', cors());
+//  app.options('*', cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGODB_URL, )
